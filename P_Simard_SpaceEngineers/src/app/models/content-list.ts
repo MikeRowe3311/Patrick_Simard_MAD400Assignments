a79680557cef67db3ba82d6c58e066d4cdbb1428
Patrick Simard <patrick.simard@stclairconnect.ca>
2023-01-23 21:39:41 -0500

    Step 11 Adding attribute and methods

--- P_Simard_SpaceEngineers/src/app/models/content-list.ts
+++ P_Simard_SpaceEngineers/src/app/models/content-list.ts
@@ -1,2 +1,30 @@
+import { isNgContent } from "@angular/compiler";
+import { IContent } from "./IContent";
+
 export class ContentList {
+
+
+private _planets: IContent[];
+
+
+constructor(){
+    this._planets = []
+}
+
+get listOfPlanets(): IContent[]{
+    return this._planets
+}
+
+// public addContent = IContent.push(_planets);
+
+public addContent(planetToAdd: IContent) {
+    this._planets.push(planetToAdd)
+}
+
+public arrayLength(){
+    this._planets.length
+}
+
+
+
 }

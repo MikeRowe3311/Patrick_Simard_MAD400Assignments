68918cddff0bc1a00af5c6195cfcd1a10b830582
Patrick Simard <patrick.simard@stclairconnect.ca>
2023-01-23 20:46:26 -0500

    Interface updated with variables

---
+++ P_Simard_SpaceEngineers/src/app/models/icontent.ts
@@ -0,0 +1,13 @@
+export interface IContent {
+
+    id: number;
+    name: string;
+    description?: string;
+    author: string;
+    imgSrc?: string;
+    type: string;
+    tags?: string[];
+
+
+
+}
